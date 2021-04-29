314f101b88399c2a9dd752c451f1dfbc42cac702
marc-town <asamin.itconsultant@gmail.com>
2021-04-29 23:29:40 +0900

    replace keyboard

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {
-  View, StyleSheet, TextInput, KeyboardAvoidingView,
+  View, StyleSheet, TextInput,
 } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButtom';
+import KeyboardSafeView from '../components/KeyboardSafeView';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,12 +26,12 @@ const styles = StyleSheet.create({
 
 export default function MemoCreateScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
