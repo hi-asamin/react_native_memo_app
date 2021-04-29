68c97c7c253b3131c1a31f0667ec6c640ee7a724
marc-town <asamin.itconsultant@gmail.com>
2021-04-30 00:00:58 +0900

    create login screen and sign up screen

--- App.jsx
+++ App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+// import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
+    // <LoginScreen />
+    <SignUpScreen />
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
     // <MemoCreateScreen />
   );
 }
